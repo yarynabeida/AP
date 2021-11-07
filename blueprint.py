5ac41536c71562b6953c7d842aaeec9fb90a495a
andriisydor <andrii.sydor.knm.2020@lpnu.ua>
2021-11-07 15:32:36 +0200

    modified blueprint

--- blueprint.py
+++ blueprint.py
@@ -1,7 +1,9 @@
-from flask import Blueprint, jsonify, request
+from flask import jsonify, request
 from schemes import UserSchema
 from models import Session, User
 from marshmallow import ValidationError
+from flask import Blueprint
+from flask_bcrypt import Bcrypt
 
 
 api_blueprint = Blueprint('api_blueprint', __name__)
@@ -13,6 +15,15 @@ def create_user():
 
     data = request.get_json()
     if not data:
+        return {"message": "No input data provided"}, 400
+
+    if data['password']:
+        print(data['password'])
+        data['password'] = Bcrypt().generate_password_hash(data['password']).decode('utf - 8')
+        print(data['password'])
+        # data['password'] = data['password'][:10]
+    else:
+        # check code + message change
         return {"message": "No input data provided"}, 400
 
     try:
@@ -37,4 +48,24 @@ def create_user():
     session.commit()
 
     result = UserSchema().dump(the_user)
+    return jsonify(result)
+
+
+@api_blueprint.route('/user/login', methods=['GET'])
+def login_user():
+    session = Session()
+
+    data = request.get_json()
+    if not data:
+        return {"message": "No input data provided"}, 400
+
+    user_find = session.query(User).filter_by(name=data['name']).first()
+    if not user_find:
+        # print(user_find)
+        return {"message": "User with such username does not exists"}, 401
+
+    if data['password'] != user_find.password:
+        return {"message": "Provided credentials are invalid"}, 401
+
+    result = UserSchema().dump(user_find)
     return jsonify(result)
